554964570739e280e6197f5e7129c2101aa9cf30
ozge.klnbck3@gmail.com <ozge.klnbck3@gmail.com>
2025-11-05 19:02:44 +0000

    ship_product

---
+++ models/staging/raw/stg_raw__ship.sql
@@ -0,0 +1,37 @@
+with 
+
+source as (
+
+    select * from {{ source('raw', 'ship') }}
+
+),
+
+renamed as (
+
+    select
+        orders_id,
+        shipping_fee,
+        shipping_fee_1,
+        logcost,
+        ship_cost
+
+    from source
+
+)
+
+select * from renamed;
+
+with
+	source as (
+		select * from {{ source('raw', 'ship') }}
+	),
+renamed as (
+	select
+		orders_id,
+		shipping_fee,
+		logcost AS log_cost,
+		CAST(ship_cost AS INT64) AS ship_cost
+	from source
+)
+
+select * from renamed
